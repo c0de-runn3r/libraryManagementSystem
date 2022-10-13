922f631f39d5219b012fe3faade172e43f538f5f
Bohdan Mandryk <bohdan.mandryk.2001@gmail.com>
2022-10-13 18:12:24 +0200

    Logger fix and api initialization

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,36 @@
 package main
 
-import . "lms/utils"
+import (
+	"os"
+
+	. "lms/controllers"
+	. "lms/utils"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo"
+)
 
 func main() {
 
-	Log("debug", "Hello World!")
+	err := godotenv.Load()
+	if err != nil {
+		Log("error", "Can't load .env file.")
+		os.Exit(1)
+	}
+
+	Log("info", "Starting LMS.")
+
+	e := echo.New()
+
+	apiGroup := e.Group("/api")
+
+	usersGroup := apiGroup.Group("/users")
+
+	booksGroup := apiGroup.Group("/books")
+
+	AttachUsersController(usersGroup)
+	AttachBooksController(booksGroup)
+
+	Log("error", e.Start(":"+os.Getenv("LMS_BACKEND_PORT")).Error())
 
 }
