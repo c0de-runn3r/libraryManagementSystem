60d7715d4af9b7c0e254496eea18a35cddbe3e2d
Bohdan Mandryk <bohdan.mandryk.2001@gmail.com>
2022-10-13 18:18:53 +0200

    idk why this changes are not commited in previous commit

---
+++ backend/controllers/books-controller.go
@@ -0,0 +1,13 @@
+package controllers
+
+import (
+	. "lms/utils"
+
+	"github.com/labstack/echo"
+)
+
+func AttachBooksController(group *echo.Group) {
+
+	Log("info", "Attaching BOOKS controller.")
+
+}
---
+++ backend/controllers/users-controller.go
@@ -0,0 +1,13 @@
+package controllers
+
+import (
+	. "lms/utils"
+
+	"github.com/labstack/echo"
+)
+
+func AttachUsersController(group *echo.Group) {
+
+	Log("info", "Attaching USERS controller.")
+
+}
