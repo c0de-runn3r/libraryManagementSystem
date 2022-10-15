cb15f87a644fe25fe48d305d8bf2239560a953d3
Toshibas <ximyko@gmail.com>
2022-10-15 22:08:19 +0300

    base SQL appended

--- backend/db/baseTable.sql
+++ backend/db/baseTable.sql
@@ -17,3 +17,16 @@ CREATE TABLE IF NOT EXISTS "books"
     "page_count" INT,
     "genres" TEXT
 );
+
+CREATE TABLE IF NOT EXISTS "authors"
+(
+    "author" VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "BooksAuthorsRel"
+(
+    "authors" VARCHAR NOT NULL,
+    "author" VARCHAR NOT NULL,
+    FOREIGN KEY "authors" REFERENCES books("authors"),
+    FOREIGN KEY "author" REFERENCES author("author")
+);
