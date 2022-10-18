1131d3d4afba0c587f50b27d875400f97368e14c
Bohdan Mandryk <bohdan.mandryk.2001@gmail.com>
2022-10-18 19:24:48 +0200

    ANOTHER ONE 2

--- backend/db/models/user.go
+++ backend/db/models/user.go
@@ -3,8 +3,8 @@ package models
 import "gorm.io/gorm"
 
 type User struct {
-	gorm.Model
 	Email    string `gorm:"not null;unique"`
 	Password string `gorm:"not null"`
 	Roles    string
+	gorm.Model
 }
