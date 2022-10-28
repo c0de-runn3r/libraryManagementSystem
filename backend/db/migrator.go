429f5070ef1ad900208d8b65e27aa5ebbe538594
c0de-runn3r <109829568+c0de-runn3r@users.noreply.github.com>
2022-10-28 22:07:50 +0200

    updated migrator

--- backend/db/migrator.go
+++ backend/db/migrator.go
@@ -13,14 +13,22 @@ func Migrate(db *gorm.DB) {
 
 	Log("info", "Auto migration starts...")
 
-	err := db.AutoMigrate(&User{})
+	err_user := db.AutoMigrate(&User{})
+	err_book := db.AutoMigrate(&Book{})
+	err_author := db.AutoMigrate(&Author{})
 
-	if err != nil {
-		Log("error", "User migration error.\n"+err.Error())
+	if err_user != nil {
+		Log("error", "User migration error.\n"+err_user.Error())
 		os.Exit(1)
 	}
-
-	// TODO добав міграцію моделей, бо толку від них немає
+	if err_book != nil {
+		Log("error", "Book migration error.\n"+err_book.Error())
+		os.Exit(1)
+	}
+	if err_author != nil {
+		Log("error", "Author migration error.\n"+err_author.Error())
+		os.Exit(1)
+	}
 
 	Log("info", "Auto migration compleate.")
 
